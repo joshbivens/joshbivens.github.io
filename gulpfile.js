3c23d7401c33c66df956bf44ce422e9ad65cc684
Josh Bivens <hotbioform@gmail.com>
2018-01-15 11:06:11 -0700

    Finish setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,42 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');  
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var prefix = require('gulp-autoprefixer');
 var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 
-gulp.task('sass', () => {  
-  gulp.src('scss/styles.scss')
-    .pipe(sass({includePaths: ['scss']}))
-    .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
-    .pipe(minifycss())
-    .pipe(gulp.dest('./dist/css'));
+gulp.task('serve', ['sass'], function() {
+  browserSync.init({
+    server: './',
+  });
+
+  gulp.watch('./scss/*.scss', ['sass']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-gulp.task('uglify', () =>{
-  gulp.src('./js/*.js')
+gulp.task('sass', function() {
+  gulp
+    .src('./scss/main.scss')
+    .pipe(sass({ includePaths: ['scss'] }))
+    .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('uglify', function() {
+  gulp
+    .src('./js/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('imagemin', () =>) {
-  gulp.src('./img/**/*')
-  .pipe(imagemin())
-  .pipe(gulp.dest('./dist/img'));
+gulp.task('imagemin', function() {
+  gulp
+    .src('./img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/img'));
 });
 
-gulp.task('browser-sync', () => {  
-  browserSync.init(["css/*.css", "js/*.js"], {
-    server: {
-      baseDir: "./"
-    }
-  });
-});
-
-gulp.task('default', ['sass', 'browser-sync', 'imagemin'], () => {  
-  gulp.watch("scss/*.scss", ['sass']);
-});
+gulp.task('default', ['serve']);
