078fc7ad5fcbd5adc638dbbfd424d4f9ad52c3c7
Josh Bivens <hotbioform@gmail.com>
2018-01-15 19:24:58 -0700

    Styles/add js

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+// When you get around to it, consider this for the slow-loading bg image
+
+// $(function(){
+//   var bgimage = new Image();      
+//   bgimage.src="http://placeimg.com/760/460/tech";       
+// $(".wrapper").hide();
+//   $(bgimage).load(function(){
+//       $(".wrapper").css("background-image","url("+$(this).attr("src")+")").fadeIn(2000);                  
+//   });
+// });
